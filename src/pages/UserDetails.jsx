396bfee8873d787ccc4e3a9bf9f1d89a9da46583
ZanaKriezi <zanakriezi@gmail.com>
2026-02-22 13:03:04 +0100

    Add user details page with routing

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,8 +1,46 @@
 import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchUsers } from "../services/api.js";
+import { Container, Typography, Card, CardContent} from "@mui/material";
+
+
 function UserDetails(){
     const {id}=useParams();
+    const [user, setUser]=useState(null);
 
-    return <div>User Details for user {id}</div>;
+    useEffect(() => {
+        const loadUser=async() => {
+            const users=await fetchUsers();
+            const selectedUser=users.find((u) => u.id === Number(id));
+            setUser(selectedUser);
+        };
+
+        loadUser();
+    }, [id]);
+
+    if(!user) {
+        return (
+            <Container sx={{mt: 4}}>
+                <Typography>Loading...</Typography>
+            </Container>
+        );
+    }
+
+    return (
+        <Container sx={{mt: 4}}>
+            <Card>
+                <CardContent>
+                  <Typography variant="h5">{user.name}</Typography>
+                  <Typography>Email: {user.email}</Typography>
+                  <Typography>Phone: {user.phone}</Typography>
+                  <Typography>Website: {user.website}</Typography>
+                  <Typography sx={{mt:2}}>
+                    Address: {user.address.street}, {user.address.city}
+                  </Typography>
+                </CardContent>
+            </Card>
+        </Container>  
+    );
+
 }
-
 export default UserDetails;
