d42c623c557b8dd59acc845d315b69782fe34983
ZanaKriezi <zanakriezi@gmail.com>
2026-02-22 12:24:44 +0100

    Add client-side search by name and email

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 import { fetchUsers }  from "../services/api";
-import {Container, Typography, Grid, Card, CardContent, CardActionArea, CircularProgress} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import { Container, Typography, Grid, Card, CardContent, CardActionArea, CircularProgress} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { TextField }  from "@mui/material";
 
 function Home(){
     const [users, setUsers]=useState([]);
     const [loading, setLoading]=useState(true);
     const navigate=useNavigate();
+    const [searchTerm, setSearchTerm]=useState("");
+
+const filteredUsers=users.filter(user =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    user.email.toLowerCase().includes(searchTerm.toLowerCase())
+);
 
     useEffect(()=> {
-        const laodUsers=async()=> {
+        const loadUsers=async()=> {
             try{
                 const data=await fetchUsers();
                 setUsers(data);
@@ -20,7 +27,7 @@ function Home(){
             }
         };
 
-        laodUsers();
+        loadUsers();
     }, []);
 
     if(loading){
@@ -37,8 +44,17 @@ function Home(){
                 User Directory
             </Typography>
 
+            <TextField 
+                fullWidth
+                label="Search by name or email"
+                variant="outlined"
+                sx={{mb: 3}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+
             <Grid container spacing={3}>
-                {users.map((user) => (
+                {filteredUsers.map((user) => (
                     <Grid item xs={12} sm={6} md={4} key={user.id}>
                         <Card>
                             <CardActionArea onClick={() => navigate(`/user/${user.id}`)}>
@@ -56,6 +72,12 @@ function Home(){
                     </Grid>        
                   ))}
             </Grid>
+
+            {filteredUsers.length===0 && (
+                <Typography variant="body1" sx={{mt: 2}}>
+                    No users found.
+                </Typography>
+            )}
       </Container>  
     );
 }
